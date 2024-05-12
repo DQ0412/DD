823f9bf387066a5adcc7db147040c353b50a240b
DQ <diemquynhqqq@gmail.com>
2024-05-12 22:07:25 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+// main.dart
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'screens/signin_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: FirebaseOptions(apiKey: 'AIzaSyBzZ46wbnrYIRLqkDMFhJB9OR0qQsgu8aQ', appId: '1:435608994173:android:5c03ba18bcbc19d8674c14', messagingSenderId: '435608994173', projectId: 'finalprojectdd'),
-
+    options: const FirebaseOptions(
+      apiKey: 'AIzaSyBzZ46wbnrYIRLqkDMFhJB9OR0qQsgu8aQ',
+      appId: '1:435608994173:android:5c03ba18bcbc19d8674c14',
+      messagingSenderId: '435608994173',
+      projectId: 'finalprojectdd',
+    ),
   );
   runApp(MyApp());
 }
@@ -19,133 +25,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginScreen(),
-    );
-  }
-}
-
-class LoginScreen extends StatefulWidget {
-  @override
-  _LoginScreenState createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-
-  @override
-  Widget build(BuildContext context) {
-    var styleFrom = ElevatedButton.styleFrom(
-      backgroundColor: Colors.blue,
-      minimumSize: Size(double.infinity, 45),
-    );
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Login Demo'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Đăng nhập',
-              style: TextStyle(
-                fontFamily: 'bold',
-                fontSize: 34,
-              ),
-            ),
-            SizedBox(height: 20),
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                hintText: 'Email',
-                filled: true,
-                fillColor: Colors.white,
-                contentPadding: EdgeInsets.fromLTRB(12, 12, 12, 12),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5),
-                  borderSide: BorderSide.none,
-                ),
-              ),
-            ),
-            SizedBox(height: 10),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                hintText: 'Mật khẩu',
-                filled: true,
-                fillColor: Colors.white,
-                contentPadding: EdgeInsets.fromLTRB(12, 12, 12, 12),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5),
-                  borderSide: BorderSide.none,
-                ),
-              ),
-              obscureText: true,
-            ),
-            SizedBox(height: 10),
-            Text('Quên mật khẩu'),
-            Divider(color: Colors.black),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _signInWithEmailAndPassword,
-              child: Text('Đăng nhập'),
-              style: styleFrom,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Future<void> _signInWithEmailAndPassword() async {
-    try {
-      final String email = _emailController.text.trim();
-      final String password = _passwordController.text.trim();
-
-      final UserCredential userCredential = await _auth.signInWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => HomeScreen()),
-      );
-    } catch (e) {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text('Error'),
-            content: Text('Failed to sign in. Please check your credentials.'),
-            actions: <Widget>[
-              TextButton(
-                child: Text('OK'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          );
-        },
-      );
-    }
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-      body: Center(
-        child: Text('Welcome!'),
-      ),
+      home: SignInScreen(),
     );
   }
 }
